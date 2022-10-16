8297b319d1076527b7891dd09c94565771d664f4
Nico Rithner <nicorithner@gmail.com>
2022-10-15 19:13:03 -0600

    Install Packer and initial plugins
    Set up packer to manage itself
    Set up command to so PackerSync runs on save

---
+++ lua/nico/plugins.lua
@@ -0,0 +1,47 @@
+local fn = vim.fn
+local status_ok, packer = pcall(require, "packer")
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+-- Automatically install packer
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+if not status_ok then
+	return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+return packer.startup(function(use)
+  -- Have packer manage itself
+  use("wbthomason/packer.nvim")
+
+ 	--------- Plugins without additional configuration
+	use("tpope/vim-fugitive") -- https://github.com/tpope/vim-fugitive
+	use("airblade/vim-gitgutter") -- https://github.com/airblade/vim-gitgutter
+	use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install" }) -- preview markdown
+	use("sbdchd/neoformat") -- formatter https://github.com/sbdchd/neoformahttps://github.com/sbdchd/neoformatt
+	use("kyazdani42/nvim-web-devicons") -- https://github.com/kyazdani42/nvim-web-devicons
+	use("nvim-lua/plenary.nvim") -- https://github.com/nvim-lua/plenary.nvim
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("tpope/vim-surround") -- changes surrounding quotes or brackets and more. https://github.com/tpope/vim-surround
+end)
