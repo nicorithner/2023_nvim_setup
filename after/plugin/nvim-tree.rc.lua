46e4b067612d4015eebd100d5996c275e49bff13
Nico Rithner <nicorithner@gmail.com>
2023-11-01 20:40:33 -0600

    Change file tree column size

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -32,7 +32,7 @@ nvim_tree.setup({ -- BEGIN_DEFAULT_OPTS
 	view = {
 		adaptive_size = false,
 		centralize_selection = false,
-		width = 40,
+		width = 25,
 		hide_root_folder = false,
 		side = "left",
 		preserve_window_proportions = false,
